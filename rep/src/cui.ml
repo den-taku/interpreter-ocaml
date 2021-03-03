d1dbf244b18460ed2e9707a99abcef4b84689169
DenTaku <dentaku1412@gmail.com>
2021-03-03 16:19:57 +0900

    Catch 'Error'

--- rep/src/cui.ml
+++ rep/src/cui.ml
@@ -3,12 +3,17 @@ open Eval
 let rec read_eval_print env =
   print_string "# ";
   flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+  try
+    let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
     let (id, newenv, v) = eval_decl env decl in
     Printf.printf "val %s = " id;
     pp_val v;
     print_newline();
     read_eval_print newenv
+  with
+    Eval.Error e -> (print_string e;
+                     print_newline();
+                     read_eval_print env)
 
 let initial_env =
   Environment.extend "i" (IntV 1)
