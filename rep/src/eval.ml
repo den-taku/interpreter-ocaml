37dd0519e18dec8f98b9b297dcaab1b8af676703
DenTaku <dentaku1412@gmail.com>
2021-03-07 22:29:09 +0900

    Modify msg when difined functions

--- rep/src/eval.ml
+++ rep/src/eval.ml
@@ -14,7 +14,7 @@ let err s = raise (Error s)
 let rec string_of_exval = function
     IntV i -> string_of_int i
   | BoolV b -> string_of_bool b
-  | ProcV _ -> "function defined"
+  | ProcV _ -> "<fun>"
 
 let pp_val v = print_string (string_of_exval v)
 
